aa244f4dabfc8b4337ed3b8bc4d7e6c32bfe4f3b
Taha Tahere <elberkani.taha@gmail.com>
2026-02-07 00:49:21 +0100

    finished the ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -33,8 +33,50 @@ Base	*generate(void)
 	return (base);
 }
 
+void	identify(Base *p)
+{
+	std::cout << "ptr base identify: ";
+	if (dynamic_cast<A*>(p))
+		std::cout << "the identified class is: A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "the identified class is: B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "the identified class is: C" << std::endl;
+	else
+		std::cout << "no type has been identified." << std::endl;
+}
+
+void	identify(Base &p)
+{
+	std::cout << "ref base identify: ";
+	try
+	{
+		dynamic_cast<A&>(p);
+		std::cout << "the identified class is: A" << std::endl;
+		return ;
+	}
+	catch (...) {}
+	try
+	{
+		dynamic_cast<B&>(p);
+		std::cout << "the identified class is: B" << std::endl;
+		return ;
+	}
+	catch (...) {}
+	try
+	{
+		dynamic_cast<C&>(p);
+		std::cout << "the identified class is: C" << std::endl;
+		return ;
+	}
+	catch (...) {}
+	std::cout << "no type has been identified." << std::endl;
+}
+
 int main(void)
 {
 	Base *obj = generate();
+	identify(obj);
+	identify(*obj);
 	return (0);
 }
